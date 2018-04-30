11cee255c5844fb7a2e7554c148872168a3be52c
Laura Bodell <bodelllaura@gmail.com>
2018-04-30 19:00:26 +0000

    automatically backed up by learn

--- app.rb
+++ app.rb
@@ -3,6 +3,19 @@ class App < Sinatra::Base
 	get '/' do
 		erb :index
 	end
+	
+	get '/hello' do 
+	  erb :hello
+	 end
+	 
+	 get '/goodbye' do
+	   erb :goodbye 
+	 end
+	 
+	 get '/date' do
+	   erd :date
+	 end 
+	 
 
 
 end
